1f8c7ed2a5620c6e264d8d7b6c5791dad462d6b3
Adilet Zainidinov <adiletzainidinov22@gmail.com>
2024-02-24 20:24:02 +0600

    ToBeContiniedd

---
+++ lesson-19 Object's/lesson.js
@@ -0,0 +1,98 @@
+// let money = 100;
+
+// function createMoney() {
+//     let money = 200;
+//     console.log(money, "fn");
+// }
+
+// createMoney();
+// console.log(money,"variable");
+
+// let money = 100;
+
+// function createMoney() {
+//     let money =200;
+//     let holiday = "Nooruz";
+// }
+
+// createMoney();
+
+// if (true){
+//     let money = "Helloo world";
+// }
+
+    // if (true){
+    //     var money = 900;
+    // }
+
+    // console.log(money);
+    // let money = 100;
+    // console.log(money);
+
+    // function message(){
+    //     var newMessageToUser = "Helloo";
+    // }
+
+    // console.log(newMessageToUser);
+
+    // let user = 1000;
+
+    // function show1() {
+    //     let user = 10;
+    //     console.log(`${user} show1`);
+    // }
+    // function show2() {
+    //     let user = 10;
+    //     console.log(`${user} show2`);
+    // }
+    // function show3() {
+    //     let user = 10;
+    //     console.log(`${user} show3`);
+    // }
+    // function show4() {
+    //     console.log(user);
+    // }
+
+    // show1()
+    // show2()
+    // show3()
+    // show4()
+
+    // showExample();
+    // thisIsArrowFunktion();
+    // thisIsExpretionFunction();
+
+    // function showExample(){
+    //     console.log('helloo');
+    // }
+
+    // const thisIsArrowFunktion = () => {
+    //     console.log('hi');
+    // }
+
+    // const thisIsExpretionFunction = function(){
+    //     console.log('privet');
+    // }
+
+    // const playerName = "Cristiano";
+    // const playerSurname = "Ronaldo";
+    // const playerBirthday = 5;
+    // const playerBirthmonth = 'feb';
+    // const playerBirthYear = 1985;
+    // const isKurguz = false;
+    // const nationality = "Portugal";
+
+    // const player = {
+    //     name: "Ronaldo",
+    //     surName: "Crestiano",
+    //     birthday: "5",
+    //     isKyz: false,
+    //     playMama: {
+    //         name: 'Mama',
+    //         age: '68'
+    //     }
+    // };
+
+    // console.log(player);
+
+    
